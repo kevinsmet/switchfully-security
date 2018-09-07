fddb602aa66c66dc6e0bd948a1cf121a47b38d99
Kevin Smet <kevin.smet@cegeka.com>
2018-09-07 11:24:05 +0200

    Opdracht A : authenthication toevoegen

--- src/main/java/com/cegeka/switchfully/security/Application.java
+++ src/main/java/com/cegeka/switchfully/security/Application.java
@@ -2,11 +2,14 @@ package com.cegeka.switchfully.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 @Import(CommonConfig.class)
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    private static Class applicationClass = Application.class;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/cegeka/switchfully/security/CommonConfig.java
+++ src/main/java/com/cegeka/switchfully/security/CommonConfig.java
@@ -1,15 +1,43 @@
 package com.cegeka.switchfully.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 
 @Configuration
-public class CommonConfig {
+@EnableWebSecurity
+public class CommonConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationEntryPoint authEntryPoint;
+
+   @Autowired
+   private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .authenticationEntryPoint(authEntryPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("JMILLER").password(passwordEncoder.encode("THANKS")).roles("USER");
+    }
 
 
     @Bean
@@ -25,5 +53,10 @@ public class CommonConfig {
                         .withCreatorVisibility(NONE)
         );
         return mapper;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
